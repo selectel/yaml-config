fd66288bd043258bbc35d8543626cacf7effef8b
Fedor Gogolev <knsd@knsd.net>
2013-09-07 12:13:10 +0400

    Documentation fixes

--- src/Data/Yaml/Config.hs
+++ src/Data/Yaml/Config.hs
@@ -4,37 +4,36 @@
 --
 -- example.yaml:
 --
--- @
--- server:
---     port: 8080
---     logs:
---         access: \/var\/log\/server\/access.log
---         error:  \/var\/log\/server\/error.log
--- @
+--
+-- > server:
+-- >     port: 8080
+-- >     logs:
+-- >         access: /var/log/server/access.log
+-- >         error:  /var/log/server/error.log
+--
 --
 -- Usage example:
 --
--- @
---module Main where
---import Prelude hiding (lookup)
---import Data.Word (Word16)
---import Data.Yaml.Config (load, subconfig, lookupDefault, lookup)
 --
---main :: IO ()
---main = do
---    config <- load "./example.yaml"
---
---    serverConfig <- subconfig config "server"
---    let interface = lookupDefault serverConfig "interface" "127.0.0.1"
---        port :: Word16 = lookupDefault serverConfig "port" 80
---
---    logConfig <- subconfig serverConfig "logs"
---    accessLog <- lookup logConfig "access"
---    errorLog <- lookup logConfig "error"
---
---    mapM_ putStrLn [interface, (show port), errorLog, accessLog]
--- @
---
+-- > module Main where
+-- > import Prelude hiding (lookup)
+-- > import Data.Word (Word16)
+-- > import Data.Yaml.Config (load, subconfig, lookupDefault, lookup)
+-- >
+-- > main :: IO ()
+-- > main = do
+-- >     config <- load "./example.yaml"
+-- >
+-- >     serverConfig <- subconfig "server" config
+-- >     let interface = lookupDefault "interface" "127.0.0.1" serverConfig
+-- >         port :: Word16 = lookupDefault "port" 80 serverConfig
+-- >
+-- >     logConfig <- subconfig "logs" serverConfig
+-- >     accessLog <- lookup "access" logConfig
+-- >     errorLog <- lookup "error" logConfig
+-- >
+-- >     mapM_ putStrLn [interface, (show port), errorLog, accessLog]
+-- 
 module Data.Yaml.Config
     ( Config
     , Key
